a8fd03cf8fa51996e0cf70063be67ac8be76fb23
Cats-n-coffee <lucie.macaigne@gmail.com>
2022-11-20 14:17:30 -0500

    add aria label to button

--- src/index.js
+++ src/index.js
@@ -32,6 +32,7 @@ const createOneCard = async ({ title, current, previous }) => {
   cardTitle.textContent = `${title}`;
 
   const cardControls = document.createElement("button");
+  cardControls.setAttribute("aria-label", "More actions");
   const cardControlsIcon = document.createElementNS(
     "http://www.w3.org/2000/svg",
     "svg"
